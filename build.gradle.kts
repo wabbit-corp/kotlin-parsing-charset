183fb18d5b0e7cf947f0b80a8f09b94d785fe69e
Sir Wabbit <559086+sir-wabbit@users.noreply.github.com>
2025-01-18 23:38:23 -0500

    Bump kotlinx-serialization-core version from 1.7.2 to 1.8.0
    Updated the kotlinx-serialization-core library to the latest version in the build.gradle.kts file.
    
    Semver Impact: MINOR

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
 
     testImplementation(kotlin("test"))
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.7.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.8.0")
 }
 
 java {
