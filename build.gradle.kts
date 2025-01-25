14c3ed3124dc123c39d425fc2bfe212f920a5bc0
Sir Wabbit <559086+sir-wabbit@users.noreply.github.com>
2025-01-25 04:14:30 -0500

    Update project version to 1.1.0
    Bump version from 1.0.1 to 1.1.0 in build.gradle.kts.
    
    Semver Impact: MINOR

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 group   = "one.wabbit"
-version = "1.0.1"
+version = "1.1.0"
 
 plugins {
     kotlin("jvm") version "2.0.20"
@@ -23,7 +23,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "one.wabbit"
             artifactId = "kotlin-parsing-charset"
-            version = "1.0.1"
+            version = "1.1.0"
             from(components["java"])
         }
     }
